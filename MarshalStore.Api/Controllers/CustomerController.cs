da8eb9d9a3858b4a9bd87cdbb0eec5ce120e74f9
Marshal Neves <marshal.neves@mprn.mp.br>
2018-04-30 16:55:51 -0300

    Create custommer controller

---
+++ MarshalStore.Api/Controllers/CustomerController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using MarshalStore.Domain.StoreContext.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarshalStore.Api.Controllers
+{
+    public class CustomerController : Controller
+    {
+        [HttpGet]
+        [Route("customers")]
+        public List<Customer> Get()
+        {
+            return null;
+        }
+
+        [HttpGet]
+        [Route("customers/{id}")]
+        public Customer GetById(Guid id)
+        {
+            return null;
+        }
+
+        [HttpGet]
+        [Route("customers/{id}/orders")]
+        public List<Order> GetOrders(Guid id)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        [Route("customers")]
+        public Customer Post([FromBody]Customer customer){
+            return null;
+        }
+
+        [HttpPut]
+        [Route("customers/{id}")]
+        public Customer Put([FromBody]Customer customer){
+            return null;
+        }
+
+        [HttpDelete]
+        [Route("customers/{id}")]
+        public string Delete(){
+            return null;
+        }
+
+    }
+}
--- MarshalStore.Api/Controllers/HomeController.cs
+++ MarshalStore.Api/Controllers/HomeController.cs
@@ -4,10 +4,33 @@ namespace MarshalStore.Api.Controllers
 {
     public class HomeController : Controller
     {
-        [Route("")]
+        [HttpGet]
+        [Route("customers")]
         public string Get()
         {
             return "Version 0.0.1";
         }
+
+        [HttpPost]
+        [Route("")]
+        public string Post()
+        {
+            return "Version 0.0.1";
+        }
+
+        [HttpPut]
+        [Route("")]
+        public string Put()
+        {
+            return "Version 0.0.1";
+        }
+
+        [HttpDelete]
+        [Route("")]
+        public string Delete()
+        {
+            return "Version 0.0.1";
+        }
+
     }
 }
